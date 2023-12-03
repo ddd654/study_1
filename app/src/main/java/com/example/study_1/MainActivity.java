985c92f52a48a482ca4b01297f4e966141c0f2e1
Coco <bound654@naver.com>
2023-12-03 16:13:13 +0900

    hi

---
+++ app/src/main/java/com/example/study_1/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.study_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView coco = findViewById(R.id.textView2);
+        coco.setText("hi");
+    }
+}
